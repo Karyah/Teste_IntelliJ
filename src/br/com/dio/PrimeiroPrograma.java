a4601163c048dcfdf7a88d455712863ae888c4dc
Karyah <mastrogiacomokaren@gmail.com>
2022-07-15 13:51:12 -0300

    Primeiro commit.

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package br.com.dio;
+
+public class PrimeiroPrograma {
+    public static int soma(int a,int b){
+        return a + b;
+
+    }
+    public static void main(String[] args) {
+
+        System.out.println("Bom Dia!!");
+        int a = 5;
+        int b = 2;
+        System.out.println(soma(a,b));
+
+    }
+
+}
